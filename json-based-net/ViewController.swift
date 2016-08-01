c8621fc1049622aaeb7934a675b220b15171d738
Kim Manuel <kimberlie.manuel@yahoo.com>
2016-08-01 14:26:16 +0800

    Initial Commit

---
+++ json-based-net/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  json-based-net
+//
+//  Created by Kim Manuel on 01/08/2016.
+//  Copyright © 2016 Kim Manuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ json-based-netUITests/json_based_netUITests.swift
@@ -0,0 +1,36 @@
+//
+//  json_based_netUITests.swift
+//  json-based-netUITests
+//
+//  Created by Kim Manuel on 01/08/2016.
+//  Copyright © 2016 Kim Manuel. All rights reserved.
+//
+
+import XCTest
+
+class json_based_netUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
